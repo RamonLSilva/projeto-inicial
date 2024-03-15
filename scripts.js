79251e0a93a2a562ffba58ed17657c30a9d4259c
RamonLSilva <ramonlimadasilva63@gmail.com>
2024-03-15 08:12:10 -0300

    projeto-inicial

--- scripts.js
+++ scripts.js
@@ -3,4 +3,10 @@ botao.addEventListener("click", alertar, false);
 
 function alertar(event){
     alert("Você clicou no botão!!! " + event);
+ 
+    var numero = 7;
+    var resultado = numero % 2;
+    if(resultado == 0){
+        alert("este número é par!");
+    }
 }
